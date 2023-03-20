3fdcd23db1fd82afc61dc09e89507e1415821fdd
Kaan ÖZDEMİR <172803062@ogr.cbu.edu.tr>
2023-03-20 17:46:07 +0300

    Add project files.

---
+++ EgitimKontrol/Controllers/KayitController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EgitimKontrol.Controllers
+{
+    public class KayitController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ EgitimKontrol/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EgitimKontrol.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
